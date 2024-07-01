5f6425a334bebb983ae36dc74f64d05d7416c014
Mahesh CHODA <choda.maheshbabu@gmail.com>
2024-07-01 18:58:12 +0530

    Create SignIn and SignOut pages.

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,31 @@
-import { SignIn } from '@clerk/nextjs';
+import { SignIn, ClerkLoading, ClerkLoaded } from '@clerk/nextjs';
+import { Loader2 } from 'lucide-react';
+import Image from 'next/image';
 
 const SignInPage = () => {
-  return <SignIn path="/sign-in" />;
+  return (
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+      <div className="h-full lg:flex flex-col justify-center items-center px-4">
+        <div className="text-center space-y-4 pt-16">
+          <h1 className="font-bold text-3xl text-[#2E2A47]">Welcome Back</h1>
+          <p className="text-base text-[#7E83A0]">
+            Login or Create account to get back to your dashboard!
+          </p>
+        </div>
+        <div className="flex justify-center items-center mt-8">
+          <ClerkLoaded>
+            <SignIn path="/sign-in" />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="" />
+          </ClerkLoading>
+        </div>
+      </div>
+      <div className="h-full bg-blue-600 hidden lg:flex justify-center items-center">
+        <Image src="/logo.svg" height={100} width={100} alt="logo" />
+      </div>
+    </div>
+  );
 };
 
 export default SignInPage;
