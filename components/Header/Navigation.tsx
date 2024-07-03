833b72fafa3b7f17573e4371e8375798ccc365c5
Mahesh CHODA <choda.maheshbabu@gmail.com>
2024-07-03 17:49:28 +0530

    Add Mobile Side menu

--- components/Header/Navigation.tsx
+++ components/Header/Navigation.tsx
@@ -1,7 +1,13 @@
 "use client";
 
+import { Menu } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
+import { useState } from "react";
+import { useMedia } from "react-use";
+
 import { NavButton } from "@/components/Header";
-import { usePathname } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 
 const navLinks = [
   {
@@ -27,7 +33,49 @@ const navLinks = [
 ];
 
 const Navigation = () => {
+  const [isOpen, setOpen] = useState(false);
   const pathName = usePathname();
+  const router = useRouter();
+
+  const isMobile = useMedia("(max-width: 1024px)", false);
+
+  const onClick = (href: string) => {
+    router.push(href);
+    setOpen(false);
+  };
+
+  if (isMobile) {
+    return (
+      <Sheet open={isOpen} onOpenChange={setOpen}>
+        <SheetTrigger>
+          <Button
+            variant="outline"
+            size="sm"
+            className="border-none bg-white/10 font-normal text-white outline-none transition hover:bg-white/20 hover:text-white"
+          >
+            <Menu className="size-4" />
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left" className="px-2">
+          <nav className="flex flex-col gap-y-2 pt-6">
+            {navLinks.map(({ href, label }) => (
+              <Button
+                key={label}
+                variant={href === pathName ? "secondary" : "ghost"}
+                onClick={(e) => {
+                  e.preventDefault();
+                  onClick(href);
+                }}
+                className="w-full justify-start"
+              >
+                {label}
+              </Button>
+            ))}
+          </nav>
+        </SheetContent>
+      </Sheet>
+    );
+  }
   return (
     <ul className="hidden items-center gap-x-2 lg:flex">
       {navLinks.map(({ href, label }) => (
