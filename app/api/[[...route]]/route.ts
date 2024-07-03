cf5525c6609c06c72edbaa882ecea2ea09c0f12f
Mahesh CHODA <choda.maheshbabu@gmail.com>
2024-07-03 12:12:47 +0530

    Setup Honojs and clerk middleware

---
+++ app/api/[[...route]]/route.ts
@@ -0,0 +1,22 @@
+import { z } from 'zod';
+import { zValidator } from '@hono/zod-validator';
+import { clerkMiddleware, getAuth } from '@hono/clerk-auth';
+import { Hono } from 'hono';
+import { handle } from 'hono/vercel';
+import { log } from 'console';
+
+export const runtime = 'nodejs';
+
+const app = new Hono().basePath('/api');
+
+app.get('/hello', clerkMiddleware(),  (context) => {
+  const auth = getAuth(context);
+  if (!auth?.userId) return context.json({ error: ' You are not authorized' });
+
+  return context.json({
+    message: 'hello there',
+  });
+});
+
+export const GET = handle(app);
+export const POST = handle(app);
