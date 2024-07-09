cb01eddaffb6be90cb6153c72d9e23f9d4b0f6ab
Mahesh CHODA <choda.maheshbabu@gmail.com>
2024-07-09 20:22:57 +0530

    Add Error handling to routes.

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -15,15 +15,20 @@ const app = new Hono()
       return c.json({ error: "Unauthorized" }, 401);
     }
 
-    const data = await db
-      .select({
-        id: accounts.id,
-        name: accounts.name,
-      })
-      .from(accounts)
-      .where(eq(accounts.userId, auth.userId));
+    try {
+      const data = await db
+        .select({
+          id: accounts.id,
+          name: accounts.name,
+        })
+        .from(accounts)
+        .where(eq(accounts.userId, auth.userId));
 
-    return c.json({ data });
+      return c.json({ data });
+    } catch (error) {
+      console.error("Error fetching accounts:", error);
+      return c.json({ error: "Internal Server Error" }, 500);
+    }
   })
   .get(
     "/:id",
@@ -46,19 +51,24 @@ const app = new Hono()
         return c.json({ error: "Missing Id" }, 400);
       }
 
-      const [data] = await db
-        .select({
-          id: accounts.id,
-          name: accounts.name,
-        })
-        .from(accounts)
-        .where(and(eq(accounts.userId, auth.userId), eq(accounts.id, id)));
+      try {
+        const [data] = await db
+          .select({
+            id: accounts.id,
+            name: accounts.name,
+          })
+          .from(accounts)
+          .where(and(eq(accounts.userId, auth.userId), eq(accounts.id, id)));
 
-      if (!data) {
-        return c.json({ error: "Not found" }, 404);
+        if (!data) {
+          return c.json({ error: "Not found" }, 404);
+        }
+
+        return c.json({ data });
+      } catch (error) {
+        console.error(`Error fetching account with id:${id}`, error);
+        return c.json({ error: "Internal Server Error" }, 500);
       }
-
-      return c.json({ data });
     },
   )
   .post(
@@ -73,15 +83,20 @@ const app = new Hono()
         return c.json({ error: "Unauthorized" }, 401);
       }
 
-      const [data] = await db
-        .insert(accounts)
-        .values({
-          userId: auth.userId,
-          ...values,
-        })
-        .returning();
+      try {
+        const [data] = await db
+          .insert(accounts)
+          .values({
+            userId: auth.userId,
+            ...values,
+          })
+          .returning();
 
-      return c.json({ data });
+        return c.json({ data });
+      } catch (error) {
+        console.error("Error posting data:", error);
+        return c.json({ error: "Internal Server Error" }, 500);
+      }
     },
   )
   .post(
@@ -96,14 +111,21 @@ const app = new Hono()
         return c.json({ error: "Unauthorized" }, 401);
       }
 
-      const data = await db
-        .delete(accounts)
-        .where(and(eq(accounts.userId, auth.userId), inArray(accounts.id, ids)))
-        .returning({
-          id: accounts.id,
-        });
+      try {
+        const data = await db
+          .delete(accounts)
+          .where(
+            and(eq(accounts.userId, auth.userId), inArray(accounts.id, ids)),
+          )
+          .returning({
+            id: accounts.id,
+          });
 
-      return c.json(data);
+        return c.json(data);
+      } catch (error) {
+        console.error("Error while performing Bulk Delete:", error);
+        return c.json({ error: "Internal Server Error" }, 500);
+      }
     },
   )
   .patch(
@@ -122,15 +144,20 @@ const app = new Hono()
 
       const values = c.req.valid("json");
 
-      const [data] = await db
-        .update(accounts)
-        .set(values)
-        .returning()
-        .where(and(eq(accounts.userId, auth.userId), eq(accounts.id, id)));
+      try {
+        const [data] = await db
+          .update(accounts)
+          .set(values)
+          .returning()
+          .where(and(eq(accounts.userId, auth.userId), eq(accounts.id, id)));
 
-      if (!data) return c.json({ error: "Not found" }, 404);
+        if (!data) return c.json({ error: "Not found" }, 404);
 
-      return c.json({ data });
+        return c.json({ data });
+      } catch (error) {
+        console.error(`Error while Updating account with id:${id}`, error);
+        return c.json({ error: "Internal Server Error" }, 500);
+      }
     },
   )
   .delete(
@@ -146,14 +173,19 @@ const app = new Hono()
 
       const { id } = c.req.valid("param");
 
-      const data = await db
-        .delete(accounts)
-        .where(and(eq(accounts.userId, auth.userId), eq(accounts.id, id)))
-        .returning({
-          id: accounts.id,
-        });
+      try {
+        const data = await db
+          .delete(accounts)
+          .where(and(eq(accounts.userId, auth.userId), eq(accounts.id, id)))
+          .returning({
+            id: accounts.id,
+          });
 
-      return c.json(data);
+        return c.json(data);
+      } catch (error) {
+        console.error(`Error while deleting account with Id:${id}:`, error);
+        return c.json({ error: "Internal Server Error" }, 500);
+      }
     },
   );
 export default app;
