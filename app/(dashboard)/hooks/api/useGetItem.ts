3d25f248d6410286bb314430e11c682614802563
Mahesh CHODA <choda.maheshbabu@gmail.com>
2024-07-15 21:35:06 +0530

    Refactor GetItem hook

--- app/(dashboard)/hooks/api/useGetItem.ts
+++ app/(dashboard)/hooks/api/useGetItem.ts
@@ -3,25 +3,43 @@ import CapTrimEnd from "@/components/utilities/CapTrimEnd";
 
 import { client } from "@/lib/hono";
 import { useQuery } from "@tanstack/react-query";
+import { InferResponseType } from "hono";
 
-interface Params {
+type TransactionResponseType = InferResponseType<
+  (typeof client.api.transactions)[":id"]["$get"],
+  200
+>["data"];
+type DefaultResponseType = InferResponseType<
+  (typeof client.api)[Exclude<ItemType, "transactions">][":id"]["$get"],
+  200
+>["data"];
+
+type ResponseType<T extends ItemType> = T extends "transactions"
+  ? TransactionResponseType
+  : DefaultResponseType;
+
+interface Params<T extends ItemType> {
   id: string;
-  itemName: ItemType;
+  itemName: T;
 }
 
-const getItem = async ({ id, itemName }: Params) => {
+const getItem = async <T extends ItemType>({
+  id,
+  itemName,
+}: Params<T>): Promise<ResponseType<T>> => {
   if (!id) {
     throw new Error(`ID is required to fetch ${CapTrimEnd(itemName, true)}.`);
   }
   const response = await client.api[itemName][":id"].$get({ param: { id } });
-  if (!response.ok)
-    throw new Error(`Failed to Fetch ${CapTrimEnd(itemName, true)}.`);
+  if (!response.ok) {
+    throw new Error(`Failed to fetch ${CapTrimEnd(itemName, true)}.`);
+  }
   const { data } = await response.json();
-  return data;
+  return data as ResponseType<T>;
 };
 
-const useGetItem = ({ id, itemName }: Params) => {
-  return useQuery({
+const useGetItem = <T extends ItemType>({ id, itemName }: Params<T>) => {
+  return useQuery<ResponseType<T>, Error>({
     enabled: !!id,
     queryKey: [`${CapTrimEnd(itemName, true)}`, { id }],
     queryFn: () => getItem({ id, itemName }),
