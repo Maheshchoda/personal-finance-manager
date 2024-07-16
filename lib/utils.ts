331598a327a78fe0fe1aa92c123423975c3af846
Mahesh CHODA <choda.maheshbabu@gmail.com>
2024-07-16 14:19:55 +0530

    Add Format Currency util.

--- lib/utils.ts
+++ lib/utils.ts
@@ -12,3 +12,11 @@ export function convertAmountToMiliUnits(amount: number) {
 export function convertAmountFromMiliUnits(amount: number) {
   return amount / 1000;
 }
+
+export function formatCurrency(amount: number) {
+  return Intl.NumberFormat("en-IN", {
+    style: "currency",
+    currency: "INR",
+    minimumFractionDigits: 2,
+  }).format(amount);
+}
