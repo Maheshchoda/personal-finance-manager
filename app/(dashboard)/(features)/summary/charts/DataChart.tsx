ffa276ad3f2a7c28dc5dcdd860faf89e556b6df1
Mahesh CHODA <choda.maheshbabu@gmail.com>
2024-07-22 19:01:35 +0530

    Add Chart Loading Skeleton Component.

--- app/(dashboard)/(features)/summary/charts/DataChart.tsx
+++ app/(dashboard)/(features)/summary/charts/DataChart.tsx
@@ -2,13 +2,23 @@
 
 import useGetSummary from "../useGetSummary";
 import CategorySpendingChart from "./CategorySpendingChart";
+import ChartLoadingSkeleton from "./ChartLoadingSkeleton";
 import TransactionsChart from "./TransactionsChart";
 
 const DataChart = () => {
   const { data, isLoading } = useGetSummary({});
 
   if (isLoading) {
-    return <div> Loading...</div>;
+    return (
+      <div className="lg:grid-col-6 grid grid-cols-1 gap-8">
+        <div className="col-span-1 lg:col-span-3 xl:col-span-4">
+          <ChartLoadingSkeleton />
+        </div>
+        <div className="col-span-1 lg:col-span-3 xl:col-span-2">
+          <ChartLoadingSkeleton />
+        </div>
+      </div>
+    );
   }
   return (
     <div className="lg:grid-col-6 grid grid-cols-1 gap-8">
