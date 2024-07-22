57a1f5f784fb137102cfa6c0e23aa5ea80f2a4c7
Mahesh CHODA <choda.maheshbabu@gmail.com>
2024-07-22 15:12:57 +0530

    Add LineChart variant component.

---
+++ app/(dashboard)/(features)/summary/charts/LineVariant.tsx
@@ -0,0 +1,52 @@
+import { format } from "date-fns";
+import {
+  Line,
+  LineChart,
+  CartesianGrid,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+} from "recharts";
+import CustomToolTip from "./CustomToolTip";
+
+interface LineVariantProps {
+  data: {
+    date: string;
+    income: number;
+    expenses: number;
+  }[];
+}
+const LineVariant = ({ data }: LineVariantProps) => {
+  return (
+    <ResponsiveContainer width="100%" height={350}>
+      <LineChart data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis
+          axisLine={false}
+          tickLine={false}
+          dataKey="date"
+          tickFormatter={(value) => format(value, "dd MMM")}
+          style={{ fontSize: "12px" }}
+          tickMargin={16}
+        />
+        <Tooltip content={<CustomToolTip />} />
+        <Line
+          dataKey="income"
+          stroke="#3d82f6"
+          strokeWidth={2}
+          className="drop-shadow-sm"
+          dot={false}
+        />
+        <Line
+          dataKey="expenses"
+          stroke="#f43f5e"
+          strokeWidth={2}
+          className="drop-shadow-sm"
+          dot={false}
+        />
+      </LineChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default LineVariant;
